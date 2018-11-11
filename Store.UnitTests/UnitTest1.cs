43b61df79561719a81742fb6aca521a020c3476e
Marcin499 <marcinkujanek90@gmail.com>
2018-11-11 12:05:44 +0100

    Commit nr 5

--- Store.UnitTests/UnitTest1.cs
+++ Store.UnitTests/UnitTest1.cs
@@ -31,31 +31,37 @@ namespace Store.UnitTests
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
 
-            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+            ProductListViewModel result = (ProductListViewModel)controller.List(2).Model;
 
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
             Assert.IsTrue(prodArray.Length == 2);
             Assert.AreEqual(prodArray[0].Name, "P4");
             Assert.AreEqual(prodArray[1].Name, "P5");
         }
 
         [TestMethod]
-        public void Can_Generate_Page_Links()
+        public void Can_Send_Pagination_View_Model()
         {
-            HtmlHelper myHelper = null;
-
-            PagingInfo pagingInfo = new PagingInfo
-            {
-                CurrentPage = 2,
-                TotalItems = 28,
-                ItemsPerPage = 10
-            };
-
-        Func<int, string> pageUrlDelegate = i => "Strona" + i;
-
-            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
-
-            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Strona1"">1</a>" + @"<a class="" btn btn-default btn-primary selected"" href=""Strona2"">2</a>" + @"<a class=""btn btn-default"" href=""Strona3"">3</a>", result.ToString());
+            // przygotowanie
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+            new Product {ProductID = 1, Name = "P1"},
+            new Product {ProductID = 2, Name = "P2"},
+            new Product {ProductID = 3, Name = "P3"},
+            new Product {ProductID = 4, Name = "P4"},
+            new Product {ProductID = 5, Name = "P5"}
+            });
+            // przygotowanie
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+            // działanie
+            ProductListViewModel result = (ProductListViewModel)controller.List(2).Model;
+            // asercje
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
         }
     }
 }
