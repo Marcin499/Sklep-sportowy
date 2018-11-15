0cf99cc4de5e142befda7ce9613e91a19758012a
Marcin499 <marcinkujanek90@gmail.com>
2018-11-15 21:04:50 +0100

    Commit nr 8

---
+++ Store.UnitTests/CartTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Store.Domain.Abstract;
+using Store.WebUI.Controllers;
+using System.Web.Mvc;
+using Store.WebUI.Models;
+using Store.Domain.Entities;
+
+namespace Store.UnitTests
+{
+    [TestClass]
+    class CartTest
+    {
+        [TestMethod]
+        public void Can_Add_To_Cart()
+        {
+            // przygotowanie — tworzenie imitacji repozytorium
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+            new Product {ProductID = 1, Name = "P1", Category = "Jab"},
+            }.AsQueryable());
+
+            // przygotowanie — utworzenie koszyka
+            Cart cart = new Cart();
+            // przygotowanie — utworzenie kontrolera
+            CartController target = new CartController(mock.Object);
+            // działanie — dodanie produktu do koszyka
+            target.AddToCart(cart, 1, null);
+
+            // asercje
+            Assert.AreEqual(cart.Lines.Count(), 1);
+            Assert.AreEqual(cart.Lines.ToArray()[0].Product.ProductID, 1);
+        }
+
+        [TestMethod]
+        public void Adding_Product_To_Cart_Goes_To_Cart_Screen()
+        {
+            // przygotowanie — tworzenie imitacji repozytorium
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+            new Product {ProductID = 1, Name = "P1", Category = "Jabłka"},
+            }.AsQueryable());
+            // przygotowanie — utworzenie koszyka
+            Cart cart = new Cart();
+            // przygotowanie — utworzenie kontrolera
+            CartController target = new CartController(mock.Object);
+            // działanie — dodanie produktu do koszyka
+            RedirectToRouteResult result = target.AddToCart(cart, 2, "myUrl");
+            // asercje
+            Assert.AreEqual(result.RouteValues["action"], "Index");
+            Assert.AreEqual(result.RouteValues["returnUrl"], "myUrl");
+        }
+
+        [TestMethod]
+        public void Can_View_Cart_Contents()
+        {
+            // przygotowanie — utworzenie koszyka
+            Cart cart = new Cart();
+            // przygotowanie — utworzenie kontrolera
+            CartController target = new CartController(null);
+            // działanie — wywołanie metody akcji Index
+            CartIndexViewModel result
+            = (CartIndexViewModel)target.Index(cart, "myUrl").ViewData.Model;
+            // asercje
+            Assert.AreSame(result.Cart, cart);
+            Assert.AreEqual(result.ReturnUrl, "myUrl");
+        }
+    }
+}
--- Store.WebUI/Controllers/CartController.cs
+++ Store.WebUI/Controllers/CartController.cs
@@ -19,49 +19,45 @@ namespace Store.WebUI.Controllers
             repository = repo;
         }
 
-        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
-        {
-            Product product = repository.Products
-                .FirstOrDefault(p => p.ProductID == productId);
-
-            if (product != null)
-            {
-                GetCart().AddItem(product, 1);
-            }
-            return RedirectToAction("Index", new { returnUrl });
-
-        }
-
-        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
-        {
-            Product product = repository.Products
-                .FirstOrDefault(p => p.ProductID == productId);
-
-            if (product != null)
-            {
-                GetCart().RemoveLine(product);
-            }
-            return RedirectToAction("Index", new { returnUrl });
-        }
-
-        private Cart GetCart()
-        {
-            Cart cart = (Cart)Session["Cart"];
-            if (cart == null)
-            {
-                cart = new Cart();
-                Session["Cart"] = cart;
-            }
-            return cart;
-        }
-
-        public ViewResult Index(string returnUrl)
+        public ViewResult Index(Cart cart, string returnUrl)
         {
             return View(new CartIndexViewModel
             {
-                Cart = GetCart(),
-                ReturnUrl = returnUrl
-            });
+                ReturnUrl = returnUrl,
+                Cart = cart
+        });
         }
+
+    public RedirectToRouteResult AddToCart(Cart cart, int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                cart.AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+
+        }
+
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                cart.RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
+        
+       
     }
 }
